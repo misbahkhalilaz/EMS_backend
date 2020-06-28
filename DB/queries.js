6f80bdd41cc260e987f3f7fa04a5564501570b90
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-06-28 14:03:14 +0500

    auth complete

--- DB/queries.js
+++ DB/queries.js
@@ -1,10 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
 
+const uri =
+	"mongodb+srv://mkaz:0438839@cluster0-ms8de.mongodb.net/EMS?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 let queryUser = (collection_name, query, filter, mapcallback) => {
-	const uri =
-		"mongodb+srv://mkaz:0438839@cluster0-ms8de.mongodb.net/EMS?retryWrites=true&w=majority";
-	const client = new MongoClient(uri, { useNewUrlParser: true });
-	return new Promise((resolve, reject) => {
+	return new Promise((resolve) => {
 		client.connect((err) => {
 			const collection = client.db("EMS").collection(collection_name);
 			resolve(collection.find(query).project(filter).toArray());
