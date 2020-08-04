16f8c86a3f82eea7030c8703fd97b9611dbf710b
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-08-05 01:13:31 +0500

    'init'

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const app = express();
+const CronJob = require("cron").CronJob;
 const auth = require("./middlewares/auth").auth;
 const express_graphql = require("express-graphql");
 const { schema } = require("./GraphQL/schema");
 const { resolver } = require("./GraphQL/resolvers");
 const cors = require("cors");
 
+let job = new CronJob(
+  "*/10 * * * * *",
+  () => console.log("hi"),
+  null,
+  false,
+  "Asia/Karachi"
+);
+
+job.start();
+
 app.use(cors());
 
 app.use(express.json(), auth);
 
 app.use(
-	"/",
-	(() =>
-		express_graphql((req) => ({
-			schema,
-			rootValue: resolver(req),
-			graphiql: false,
-		})))()
+  "/",
+  (() =>
+    express_graphql((req) => ({
+      schema,
+      rootValue: resolver(req),
+      graphiql: false,
+    })))()
 );
 
 app.listen(4000, () => {
-	console.log("port = 4000");
+  console.log("port = 4000");
 });
